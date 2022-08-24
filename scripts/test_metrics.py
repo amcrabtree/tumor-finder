a3801b77d6ccf1325adffc58a4bfe139b8a1c075
amcrabtree <acrabtre@uoregon.edu>
2022-08-23 17:19:34 -0700

    debugging test scripts, adding metrics

--- scripts/test_metrics.py
+++ scripts/test_metrics.py
@@ -12,9 +12,10 @@ import json
 import pandas as pd
 from tumor_utils.data import TiledDataset # custom dataset class 
 from tumor_utils.test import Tester # custom class for testing
-from torch.utils.data import DataLoader
+from tumor_utils import data, tformer
 import torch
-from torchvision import transforms
+from torch.utils.data import DataLoader
+
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 if __name__=='__main__':
@@ -24,47 +25,48 @@ if __name__=='__main__':
     config = json.loads(config_file.read())
     config_file.close()
 
+    # set full output filenames
+    project_dir = os.path.join(config['run_dir'], config['run_name'])
+    for el in config['output']:
+        config['output'][el] = os.path.join(project_dir, config['output'][el])
+
     # 1. import datasets with custom Dataset class
-    label_transform = transforms.Lambda(lambda y: torch.tensor(y, dtype=torch.float))
-    img_transform = transforms.Compose([
-        transforms.CenterCrop(224),
-        transforms.PILToTensor(),
-        transforms.ConvertImageDtype(torch.float), 
-        transforms.Normalize(
-            mean=[0.48235, 0.45882, 0.40784], 
-            std=[0.00392156862745098, 0.00392156862745098, 0.00392156862745098]),
-    ])
+    image_transform = tformer.custom_tfm(
+            data='image', model=config['model']['arch'], input_size=256)
+    label_transform = tformer.custom_tfm(
+            data='label', model=config['model']['arch'], input_size=256)
     # define and transform datasets 
-    test_set = TiledDataset(
-        set_dir = os.path.join(config['tile_dir'], "test"),
-        transform=img_transform,
-        target_transform=label_transform
-    )
+    test_set = getattr(data, config['data_loader']['type'])(
+        set_dir = os.path.join(
+            config['data_loader']['args']['data_dir'], 
+            config['data_loader']['args']['test_subd']),
+        transform = image_transform,
+        target_transform = label_transform)
 
     # 2. load datasets into dataloaders
     test_loader = DataLoader(
         test_set, 
-        batch_size=config['batch_size'], 
-        shuffle=True, 
-        drop_last=True)
+        batch_size = config['data_loader']['args']['batch_size'], 
+        num_workers = 1, 
+        pin_memory = True,
+        shuffle = False, 
+        drop_last = True)
 
     # 3. Load trained model from file
-    model = torch.load(config['model_file'])
+    model = torch.load(config['output']['running_model'])
     model.to(device)
+
+    # save test summary to file
+    old_stdout = sys.stdout # save original stdout
+    test_log = open(config['output']['test_log'],"w")
+    sys.stdout = test_log
 
     # 4. Make predictions (perform forward pass using test images)
     print("\nTesting model ...\n")
     model.eval() # puts model into inference mode
-    trainer = Tester(model, config)
-    trainer.test(test_loader)
+    tester = Tester(model, config)
+    tester.test(test_loader)
 
-    # 5. Save the training stats (loss and accuracy) to CSV
-    stats_df = pd.DataFrame.from_dict(trainer.stats_list)
-    project_dir = os.path.join(config['out_dir'], config['run_name'])
-    stats_file = os.path.join(project_dir, "test_stats.csv")
-    stats_df.to_csv(stats_file, index=False)
-    print(f'\n\tSaved stats file to: \n\t\t{stats_file}\n')
-
-    # 6. Generate Plots
-    ## confusion matrix
-    #confusion_plot(y_pred, y_test)
+    # close logfile and reset stdout
+    sys.stdout = old_stdout
+    test_log.close()
--- scripts/tumor_utils/test.py
+++ scripts/tumor_utils/test.py
@@ -1,6 +1,8 @@
 import time
 import torch
 import torch.nn as nn
+import torchmetrics as TM
+import pandas as pd
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -8,28 +10,28 @@ class Tester():
     """ Tests a model 
     """
     def __init__(self, model, config):
-        self.config = config
         self.model = model
+        self.config = config
+        self.metrics = list(config['tester']['metrics'])
+        self.criterion = getattr(torch.nn.modules.loss, config['loss'])()
         self.stats_list = []
-        self.test_loss = 0.0
         self.test_acc = 0.0
-    
-    def loss_fn(self, inputs, labels):
-        criterion = nn.CrossEntropyLoss()
-        outputs = self.model(inputs)
-        loss = criterion(outputs, labels.long()) # .long converts to scalar
-        return loss
 
     def accuracy_fn(self, inputs, labels):
         """ Returns batch accuracy. 
         """
         outputs = self.model(inputs)
         _, preds = torch.max(outputs, dim=1) # best prediction (max prob.)
-        true_count = 0
-        for i in range(len(preds)):
-            pred_is_accurate = (preds[i] == labels.data[i]).data
-            if pred_is_accurate:
-                true_count += 1
+        true_count = torch.sum(torch.eq(preds, labels))
+        accuracy = true_count / len(preds)
+        return accuracy
+
+    def accuracy_fn(self, inputs, labels):
+        """ Returns batch accuracy. 
+        """
+        outputs = self.model(inputs)
+        _, preds = torch.max(outputs, dim=1) # best prediction (max prob.)
+        true_count = torch.sum(torch.eq(preds, labels))
         accuracy = true_count / len(preds)
         return accuracy
 
@@ -37,26 +39,40 @@ class Tester():
         since = time.time() # track time
         with torch.no_grad():
             torch.set_grad_enabled(False)
-            step_loss = []
-            running_loss = 0.0
-            running_accuracy = 0
+            accuracy = 0.0
+            f1_score = 0.0
+            auroc = 0
+
             for step, (images, labels) in enumerate(test_loader):
                 images, labels = [images.to(device), labels.to(device)]
-                loss = self.loss_fn(images, labels)
-                step_loss.append(loss.item())
-                running_loss += loss.item() #* images.size(0)
-                batch_acc = self.accuracy_fn(images, labels)
-                running_accuracy += batch_acc
+                outputs = self.model(images)
 
-            ## print and save loss & acc
-            self.test_loss = running_loss / len(test_loader)
+                y_pred = outputs.argmax(-1).cpu()
+                #y_pred = outputs
+                y_tgt = labels.long().cpu()
+
+                accuracy = TM.Accuracy()(y_pred, y_tgt)
+                f1_score = TM.F1Score()(y_pred, y_tgt)
+                prere = TM.functional.precision_recall(y_pred, y_tgt)
+                print(f'Batch: {step} Acc: {accuracy:.4f} f1_score: {f1_score:.4f} auroc: {prere}')
+
+            time_elapsed = time.time() - since
+            print(f'Testing complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+
+"""
+            ## METRICS
             self.test_acc = running_accuracy / len(test_loader)
-            self.stats_list.append(  # save loss info in stats_list attribute
-                {'phase':'test',
-                'test_loss':self.test_loss,
-                'test_acc':self.test_acc
-                })
-            print(f'test Loss: {self.test_loss:.4f} Acc: {self.test_acc:.4f}')
+            self.precision = FM.precision_recall(images, labels, average='micro')
 
-        time_elapsed = time.time() - since
-        print(f'Testing complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+            ## save metrics to stats dict
+            self.stats_list.append(  # save loss info in stats_list attribute
+                {'model':self.model.__class__.__name__,
+                'phase':'test',
+                'acc':self.test_acc,
+                })
+            print(f'test Acc: {self.test_acc:.4f}')
+
+            ## save stats info to file
+            stats_df = pd.DataFrame.from_dict(self.stats_list)
+            stats_df.to_csv(self.config['output']['metrics_file'], index=False)
+"""
